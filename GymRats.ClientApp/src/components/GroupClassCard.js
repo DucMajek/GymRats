74df175fae2409f5e4cdb29c480198648c730c84
Your Name <ducanhdinh00@gmail.com>
2025-05-28 22:06:15 +0200

    frontend zmiana

--- GymRats.ClientApp/src/components/GroupClassCard.js
+++ GymRats.ClientApp/src/components/GroupClassCard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 export default function GroupClassCard({
   type,
@@ -8,16 +8,37 @@ export default function GroupClassCard({
   date,
   coachName,
   signedIn,
-  onSignIn
+  onSignIn,
+  onDrop
 }) {
+  const [showDropModal, setShowDropModal] = useState(false);
+
+  const handleConfirmDrop = () => {
+    onDrop();
+    setShowDropModal(false);
+  };
+
   return (
     <div className="group-card">
+      
       <p className="group-card-type">{type}</p>
       <h3 className="group-card-title">{title}</h3>
 
       <div className="group-card-middle">
         {signedIn ? (
-          <span className="signin-status">Zapisany ✓</span>
+          <div className="signed-container" >
+            <div className="signed-icon">
+            <span className="signin-status">Zapisany ✓</span>
+            </div>
+            <Button
+              variant="danger"
+              size="sm"
+              className="mt-2"
+              onClick={() => setShowDropModal(true)}
+            >
+              Rezygnuj
+            </Button>
+          </div>
         ) : (
           <button className="sign-in-button" onClick={onSignIn}>
             Zapisz się
@@ -30,6 +51,27 @@ export default function GroupClassCard({
         <span className="stats-coach">{coachName}</span>
         <span className="stats-size">{date}</span>
       </div>
+
+      <Modal
+        show={showDropModal}
+        onHide={() => setShowDropModal(false)}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Potwierdź rezygnację</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Czy na pewno chcesz zrezygnować z tych zajęć?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowDropModal(false)}>
+            Anuluj
+          </Button>
+          <Button variant="danger" onClick={handleConfirmDrop}>
+            Potwierdź
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
